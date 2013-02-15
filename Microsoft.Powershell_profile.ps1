d1150e377aa73932efebde1f1aa9ef6360d7334e
rafaelgp <mr.rafaelgp@gmail.com>
2013-02-15 17:18:37 +0000

    Move all git configuration to conf/posh_git.ps1 and source it

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -4,11 +4,8 @@ $posh_dir = "$ENV:USERPROFILE\Documents\WindowsPowerShell"
 # Add defun directory to PowerShell path  
 $env:Path = $env:Path + ";$posh_dir\defun\"
 
-Import-Module $posh_dir\modules\posh_git
-
 # Appearance
 . "$posh_dir\conf\posh_appearance.ps1"
+. "$posh_dir\conf\posh_git.ps1"
 # Aliases
 . "$posh_dir\conf\posh_aliases.ps1"
-
-Enable-GitColors
---
+++ conf/posh_git.ps1
@@ -0,0 +1,23 @@
+﻿Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+Import-Module $posh_dir\modules\posh_git
+
+# Set up tab expansion and include git expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    
+    switch -regex ($lastBlock) {
+        # Execute git tab completion for all git-related commands
+        'git (.*)' { GitTabExpansion $lastBlock }
+        # Fall back on existing tab expansion
+        default { DefaultTabExpansion $line $lastWord }
+    }
+}
+
+if(-not (Test-Path Function:\DefaultTabExpansion)) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+
+Enable-GitColors
+
+Pop-Location
