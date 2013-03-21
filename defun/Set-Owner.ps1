aa0eddff6e8878d11d75dd90592c7a0253c8b6c6
rafaelgp <mr.rafaelgp@gmail.com>
2013-03-21 15:10:11 +0000

    New script -> set-owner.sh, which helps to set a new owner of a file

---
+++ defun/Set-Owner.ps1
@@ -0,0 +1,5 @@
+﻿# Usage: set-owner [user] [file]
+$objUser = New-Object System.Security.Principal.NTAccount($args[0])
+$objFile = Get-Acl $args[1]
+$objFile.SetOwner($objUser)
+Set-Acl -aclobject $objFile -path $args[1]
