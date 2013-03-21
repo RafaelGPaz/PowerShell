aa89515fdfd5c8a81e958db7e5adce8d9186e9ce
rafaelgp <mr.rafaelgp@gmail.com>
2013-03-21 15:07:06 +0000

    New script -> set-perm.ps1, which is the equivalent of chmod u+wrk

---
+++ defun/Set-Perm.ps1
@@ -0,0 +1,6 @@
+﻿# This script is the equivalent to chmod u+wrk, it adds adds full permissions only to the user
+# It's not the equivalent to chmod 700
+$Acl = Get-Acl "$args"
+$Ar = New-Object  system.security.accesscontrol.filesystemaccessrule("rafaelgp","FullControl","Allow")
+$Acl.SetAccessRule($Ar)
+Set-Acl "$args" $Acl
