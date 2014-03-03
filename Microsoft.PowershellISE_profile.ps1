0be407a0c22e874c68bcfdb176daa9eca20ceb63
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-03 11:45:30 +0000

    Romove code to use posh-git i manually installed, as now I'm using the posh-git module

--- Microsoft.PowershellISE_profile.ps1
+++ Microsoft.PowershellISE_profile.ps1
@@ -1,2 +1,5 @@
 # Load ConsoleHost profile
-. 'C:\Users\rafaelgp\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1'
+. 'C:\Users\rafaelgp\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1'
+# Load posh-git example profile
+#. 'C:\Users\rafaelgp\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
+
