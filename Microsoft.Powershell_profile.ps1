59ec4853e020c5d326cfeb7da5d01ce5cc2bfce6
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-03 11:45:56 +0000

    Edited Powershell profile

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -3,25 +3,33 @@ $posh_dir = "$ENV:USERPROFILE\Documents\WindowsPowerShell"
 
 # Add defun directory to PowerShell path  
 $env:Path = $env:Path + ";$posh_dir\defun\"
+$env:Path = $env:Path + ";$posh_dir\defun\newvt\"
+$env:Path = $env:Path + ";$posh_dir\defun\newvt\lib\"
 
 # Staff only for PoSH Console
 if ( $Host.Name -eq "ConsoleHost" )
 {
-# Import PowerTab module before PoshGit
+#PoshGit already imports it's own PowerTab module
 Import-Module PowerTab
-Import-Module PoshGit
+#Import-Module Posh-Git
+Import-Module PsGet
+Import-TabExpansionTheme Digital
 }
 
 #Staff only for PoSH ISE
 if ( $Host.Name -eq "Windows PowerShell ISE Host")
 {
 Import-Module TabExpansion++
+Import-Module PsGet
 }
-
 
 # Appearance
 . "$posh_dir\conf\posh_appearance.ps1"
-#. "$posh_dir\conf\posh_git.ps1"
 # Aliases
 . "$posh_dir\conf\posh_aliases.ps1"
-
+# Global Variables
+. "$posh_dir\conf\posh_global_var.ps1"
+# Vim
+. "$posh_dir\conf\posh_vim.ps1"
+# Emacs
+. "$posh_dir\conf\posh_emacs.ps1"
