af24cd8b79c9390dadade0b07e4ace3340d0419f
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-03 11:43:14 +0000

    Changed appearance

--- conf/posh_appearance.ps1
+++ conf/posh_appearance.ps1
@@ -1,32 +1,38 @@
 ﻿# PowerShell appearance
 
-# Set up a simple prompt, adding the git prompt parts inside git repos
-function prompt {
-    Write-Host($pwd) -nonewline -ForegroundColor Yellow
-    # Git Prompt
-    if($Global:GitPromptSettings.EnablePromptStatus) {
-        $Global:GitStatus = Get-GitStatus
-        Write-GitStatus $GitStatus
-    }
-    return "> "
+# Set up a simple prompt (not in PowerGUI) showing the current directory
+if ( $Host.Name -ne "PowerGUIScriptEditorHost" )
+{
+	function prompt {
+	    Write-Host PS ($pwd) -nonewline -ForegroundColor Green
+	    # Git Prompt
+	    if($Global:GitPromptSettings.EnablePromptStatus) {
+	        $Global:GitStatus = Get-GitStatus
+	        Write-GitStatus $GitStatus
+	    }
+	    return "> "
+	}
 }
 
+# Set Terminal colors
 if ( $Host.Name -eq "ConsoleHost" )
 {
+
+$bgcolor = "Black"
+
 $rui = (Get-Host).UI.RawUI
-$rui.BackgroundColor = "DarkBlue"
+$rui.BackgroundColor = "$bgcolor"
 $rui.ForegroundColor = "White"
 
-$bgcolor = "DarkBlue"
 $pdata = (Get-Host).PrivateData
-$pdata.ErrorForegroundColor    = "Green"
+$pdata.ErrorForegroundColor    = "Red"
 $pdata.ErrorBackgroundColor    = "$bgcolor"
-$pdata.WarningForegroundColor  = "DarkRed"
+$pdata.WarningForegroundColor  = "Magenta"
 $pdata.WarningBackgroundColor  = "$bgcolor"
 $pdata.DebugForegroundColor    = "Cyan"
 $pdata.DebugBackgroundColor    = "$bgcolor"
 $pdata.VerboseForegroundColor  = "Yellow"
 $pdata.VerboseBackgroundColor  = "$bgcolor"
-$pdata.ProgressForegroundColor = "Yellow"
+$pdata.ProgressForegroundColor = "Cyan"
 $pdata.ProgressBackgroundColor = "Blue"
 }
