60abac9e51a76e42e81cd7cee9088c1bd3548b11
rafaelgp <mr.rafaelgp@gmail.com>
2014-03-03 11:44:33 +0000

    Added a file to storage all the Global variables

---
+++ conf/posh_global_var.ps1
@@ -0,0 +1,13 @@
+﻿$jb = "E:\jobs"                                        # JoBs
+$vt = "E:\virtual_tours"                               # Virtual Tours
+$wb = "E:\websites"                                    # WeBsites
+$ss = "E:\360_site_survey"                             # 360 Site Survey       
+
+$wp = "$ENV:HOMEPATH\Documents\WindowsPowerShell"      # Windows Powershell
+
+$dk = "$ENV:HOMEPATH\Desktop"                          # DesKtop
+$dw = "$ENV:HOMEPATH\Downloads"                        # DoWnloads
+$dc = "$ENV:HOMEPATH\Documents"                        # DoCuments
+$bx = "$ENV:HOMEPATH\Dropbox"                          # drop BoX
+
+$hm = "$ENV:HOME"                                      # HoMe
