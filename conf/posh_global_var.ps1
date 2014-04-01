9afc5f35a0e94a0a3faef9778a9160a1497408ff
rafaelgp <mr.rafaelgp@gmail.com>
2014-04-01 13:58:37 +0100

    Fix shortcus to all the directories in my home.
    $env:homepath is not relative to C:/ so I had to create a new variable
    that joins $homedrive and $homepath

--- conf/posh_global_var.ps1
+++ conf/posh_global_var.ps1
@@ -1,13 +1,14 @@
 ﻿$jb = "E:\jobs"                                        # JoBs
 $vt = "E:\virtual_tours"                               # Virtual Tours
 $wb = "E:\websites"                                    # WeBsites
-$ss = "E:\360_site_survey"                             # 360 Site Survey       
 
-$wp = "$ENV:HOMEPATH\Documents\WindowsPowerShell"      # Windows Powershell
-
-$dk = "$ENV:HOMEPATH\Desktop"                          # DesKtop
-$dw = "$ENV:HOMEPATH\Downloads"                        # DoWnloads
-$dc = "$ENV:HOMEPATH\Documents"                        # DoCuments
-$bx = "$ENV:HOMEPATH\Dropbox"                          # drop BoX
-
+$hmm = $ENV:HOMEDRIVE + $ENV:HOMEPATH                  # HoMMe 
 $hm = "$ENV:HOME"                                      # HoMe
+
+$wp = "$hmm\Documents\WindowsPowerShell"      # Windows Powershell
+
+$dk = "$hmm\Desktop"                          # DesKtop
+$dw = "$hmm\Downloads"                        # DoWnloads
+$dc = "$hmm\Documents"                        # DoCuments
+$bx = "$hmm\Dropbox"                          # drop BoX
+
