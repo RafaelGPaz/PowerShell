cc0cd0359c0ca47b859399f318543af12ab1845a
RafaelGP <mr.rafaelgp@gmail.com>
2014-10-16 16:40:37 +0100

    Delete a couple of old scripts

--- defun/Get-Age.ps1
+++
@@ -1,6 +0,0 @@
-﻿[datetime]$birthday = "12/22/2012 03:27:00"
-$span = [datetime]::Now - $birthday
-Write-Host $span.Days
-$age = New-Object DateTime -ArgumentList $Span.Ticks
-write-host $age
-Write-Host "My daughter's age is:" $($age.Year -1) Years $($age.Month -1) Months $age.Day "days" ($age.Hour) "hours" ($age.Minute) "minutes" ($age.second) "seconds" 
