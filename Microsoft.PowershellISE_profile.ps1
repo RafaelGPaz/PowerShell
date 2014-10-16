001226182e85b5ad5ae3bd9b46886109fbc83664
RafaelGP <mr.rafaelgp@gmail.com>
2014-10-16 16:45:18 +0100

    Update ISE profile

--- Microsoft.PowershellISE_profile.ps1
+++ Microsoft.PowershellISE_profile.ps1
@@ -1,5 +1,1 @@
-# Load ConsoleHost profile
-. 'C:\Users\rafaelgp\Documents\WindowsPowerShell\Microsoft.Powershell_profile.ps1'
-# Load posh-git example profile
-#. 'C:\Users\rafaelgp\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
-
+# Load ConsoleHost profile. "$(split-path $PROFILE)\Microsoft.Powershell_profile.ps1"
