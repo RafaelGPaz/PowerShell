594cdc133d6bc1aa25073816397a0d997c029a5a
RafaelGP <mr.rafaelgp@gmail.com>
2014-10-20 11:24:11 +0100

    Change the output folder name and the way to check if it exists

--- Scripts/Make-Thumbs.ps1
+++ Scripts/Make-Thumbs.ps1
@@ -7,19 +7,19 @@
 
 Clear-Host
 
-$check_dir = Test-Path -PathType Container thumbs
-if($check_dir -eq $false){
-    New-Item thumbs -type directory | Out-Null
+$thumbsDir = '.\scroll_thumbs'
+if (!(Test-Path $thumbsDir)) {
+    New-Item $thumbsDir -type directory | Out-Null
 }
 
 $all_img = dir ".\*.jpg"
 Write-Host [ info ] Number of images: $all_img.count -ForegroundColor Blue
 foreach ( $img_in in dir $all_img ) { 
     $img_out = $($img_in).name
-    $check_thumb = Test-path -PathType Leaf thumbs\$img_out
+    $check_thumb = Test-path -PathType Leaf $thumbsDir\$img_out
     if($check_thumb -eq $false) {   
         write-host "[ info ] Creating thumb: $img_out ..." -ForegroundColor Blue
-        Invoke-Expression "C:\'Program Files (x86)'\ImageMagick-6.8.9-Q16\convert.exe $img_in -resize 420x210^ -gravity center -extent 200x120 thumbs\$img_out"
+        Invoke-Expression "C:\'Program Files (x86)'\ImageMagick-6.8.9-Q16\convert.exe $img_in -resize 420x210^ -gravity center -extent 200x120 $thumbsDir\$img_out"
         write-host "[  ok ] $img_out" -ForegroundColor Green
     }
     else {
