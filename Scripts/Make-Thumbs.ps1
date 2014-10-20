c086573bd8ee3b9b518edec04b3ecb2a47640547
RafaelGP <mr.rafaelgp@gmail.com>
2014-10-20 13:23:19 +0100

    Masive improvement of the Make-tiles script

--- Scripts/Make-Thumbs.ps1
+++ Scripts/Make-Thumbs.ps1
@@ -1,29 +1,35 @@
 ﻿<#
 .DESCRIPTION
- Creates thumbnails to use by the scroll feature (420 x 210)
- IMPORTANT: Go to the directory where all the jpg files are before running the script
- The output folder is scroll_panos
+    Create 420 x 210 thumbnails to use by the scroll feature
+    Run the script from the tour's root directory
+    Mandatory param: tour's folder
+.EXAMPLE
+    Make-Thumbs bugatti_showroom/
 #>
-
-Clear-Host
-
-$thumbsDir = '.\scroll_thumbs'
-if (!(Test-Path $thumbsDir)) {
-    New-Item $thumbsDir -type directory | Out-Null
-}
-
-$all_img = dir ".\*.jpg"
-Write-Host [ info ] Number of images: $all_img.count -ForegroundColor Blue
-foreach ( $img_in in dir $all_img ) { 
-    $img_out = $($img_in).name
-    $check_thumb = Test-path -PathType Leaf $thumbsDir\$img_out
-    if($check_thumb -eq $false) {   
-        write-host "[ info ] Creating thumb: $img_out ..." -ForegroundColor Blue
-        Invoke-Expression "C:\'Program Files (x86)'\ImageMagick-6.8.9-Q16\convert.exe $img_in -resize 420x210^ -gravity center -extent 200x120 $thumbsDir\$img_out"
-        write-host "[  ok ] $img_out" -ForegroundColor Green
-    }
-    else {
-        write-host "[  ok  ] $img_out" -ForegroundColor Green
-    }
-}
-write-host [ ---- ] End -ForegroundColor Green 
+    [CmdletBinding()]
+    [Parameter(Mandatory=$true, Position=0)]
+    Param ($tourDir)
+Write-Verbose "Starting..."
+# Check the script is running in the right directory
+if (!(Test-Path '.\.src')) { Throw "Cant find folder '.src'. Are you sure you're in the right directory?" }
+# Create 'scroll_panos' directory
+$thumbsDir = ".\$tourDir\files\content\scroll_thumbs"
+if (!($thumbsDir)) { mkdir $thumbsDir }
+# Create 'scroll_panos' directory for devel
+$develThumbs = ".\$tourDir\devel\files\content\scroll_thumbs"
+if (!($develThumbs)) { mkdir $develThumbs }
+Get-ChildItem ".\.src\panos\$tourDir" -Filter "*.jpg" |
+        foreach {
+            $destThumb = "$thumbsDir\$($_.name)"
+            # Create a thumbnail for each scene, if it doesn't exist already
+            if (!(Test-Path $destThumb)) {
+                Write-Verbose "Creating $destThumb ..."
+                Invoke-Expression "C:\'Program Files (x86)'\ImageMagick-6.8.9-Q16\convert.exe $($_.FullName) -resize 420x210^ -gravity center -extent 200x120 $destThumb"                
+            }
+            # Copy each thumbnail to the devel directory if it's not already there
+            if (!(Test-Path "$develThumbs\$($_.name)")) {
+                copy $destThumb $develThumbs
+                Write-Verbose "Copy $($_.name) to devel directory"
+            }
+        }
+Write-Verbose "End of line."
