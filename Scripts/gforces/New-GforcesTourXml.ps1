b50448b2cf0ea787eb9f929e605dc02dd8e8a1f4
RafaelGP <mr.rafaelgp@gmail.com>
2014-11-25 21:12:20 +0000

    New-GforcesTourXml > First commit

---
+++ Scripts/gforces/New-GforcesTourXml.ps1
@@ -0,0 +1,108 @@
+﻿<#
+.DESCRIPTION
+    Create a new tour.xml file merging all the XML files in the following directories: 
+    - files/content
+    - ../../shared/include
+    - files/include (if exists)
+    - files/scenes
+.EXAMPLE
+    New-GforcesTourXml  .\lexus_rc_topaz-brown_2015\
+    New-GforcesTourXml  .\lexus_rc_topaz-brown_2015\ -Verbose
+    Get-ChildItem .\*lexus | New-GforcesTourXml
+#>
+[CmdletBinding()]             
+    Param (                        
+        [Parameter(
+            Mandatory=$False, 
+            ValueFromPipeline=$true)]
+        [String[]]$Param1,
+        [String] $IncludeFolder
+        )
+
+Begin {
+    Clear-Host
+    Write-Verbose "Start"
+
+    function Add-IncludeXmlFiles ($IncludePath,$basedir) {
+    Write-Verbose ">  $IncludePath"
+    $include = Get-ChildItem $IncludePath\*\*.xml -Exclude 'coordfinder', 'editor_and_options'
+    if($include -eq $null) { rm $tourfile; Throw "There are no xml files in the 'include' directory" }
+    Write-Verbose '>> Include'
+    #Write-Verbose =  $include.Directory.Name
+    ForEach ($file in $include) {
+    #if ($IncludePath -eq "$Param1\files\include") {
+    #Write-Verbose "   $($file.Directory.Name)/index.xml"
+    #} else {
+    #    Write-Verbose "   shared/$($file.Directory.Name)/index.xml"
+    #}
+    Write-Verbose "   $basedir/$IncludeFolder/$($file.Directory.BaseName)"
+    #Write-Host $IncludeFolder
+    Add-Content $tourfile $(Get-Content $file.FullName | 
+    where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_ -notmatch '<?xml version' -and $_.trim() -ne "" } |
+    foreach {$_.ToString().TrimStart() }  
+    Out-String ) }    
+    }
+}
+Process {
+    # Remove '.' and '\' from variable
+    $Param1 = $Param1.replace('.', '')
+    $Param1 = $Param1.replace('\', '')
+    foreach ($TourPath in $Param1) {
+        Write-Verbose "Path: $TourPath"
+        if(!(Test-Path -Path "$TourPath\files")) { Throw "There is no 'files' directory. Are you in the right directory?" }
+        $tourfile="$TourPath\files\tour.xml"
+        if(!(Test-Path -Path $tourfile)) {
+        touch $tourfile
+        } else {
+        rm $tourfile
+        }
+        # Head
+        set-content $tourfile  '<krpano version="1.18" showerrors="false"><krpano logkey="true" />'
+
+        # Content Directory
+        if(!(Test-Path -Path "$TourPath\files\content")) { rm $tourfile; Throw "There is no 'content' directory" 
+        }
+        $content = Get-ChildItem $TourPath\files\content\*.xml
+        if($content -eq $null) { rm $tourfile; Throw "There are no xml files in the 'content' directory" }
+        Write-Verbose '>> Content'
+        Write-Verbose "    $($content.Name)"
+        ForEach ($file in $content) {Add-Content $tourfile $(Get-Content $file.FullName | 
+        where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_ -notmatch '<?xml version' -and $_.trim() -ne "" } |
+        foreach {$_.ToString().TrimStart() }  
+        Out-String) }
+
+        # Incude Directory
+        #Add-IncludeXmlFiles -IncludePath "$TourPath\files\include"
+        #Add-IncludeXmlFiles -IncludePath "$TourPath\..\shared\$IncludeFolder"
+        # Make 'include' the default value of $IncludeFolder
+        if($IncludeFolder -eq '') { $IncludeFolder='include' }
+        # Add XML files
+        if(Test-Path -Path "$TourPath\files\include") {
+            Add-IncludeXmlFiles -IncludePath "$TourPath\files\$IncludeFolder"
+        } else {
+            if(!(Test-Path -Path "$TourPath\..\shared\$IncludeFolder")) { Throw "The directory shared/$IncludeFolder doesn't exist." }
+            Add-IncludeXmlFiles -IncludePath "$TourPath\..\shared\$IncludeFolder" -basedir "shared"
+        }
+
+        # Scenes Directory
+        if(!(Test-Path -Path "$TourPath\files\scenes")) { rm $tourfile; Throw "There is no 'scenes' directory" }
+        #$scenes = Get-ChildItem .\files\scenes\*.xml
+        $scenes = Get-ChildItem $TourPath\files\scenes\*.xml -Exclude info*.xml
+        if($scenes -eq $null) { rm $tourfile; Throw "There are no xml files in the 'scenes' directory" }
+        Write-Verbose '>> Scenes'
+        Write-Verbose "    $($scenes.Name)"
+        ForEach ($file in $scenes) {
+        # Open scene tag
+        #Add-Content  $tourfile "<scene name=`"$($_.BaseName)`">";  
+        # scenes\scene#.xml
+        Add-Content $tourfile $(Get-Content $file.FullName |
+        where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_.trim() -ne "" } |
+        #foreach {$_.ToString().TrimStart().Replace("url=`"scene","url=`"`%SWFPATH`%/scenes/scene") } |
+        foreach {$_.ToString().TrimStart() } |
+        Out-String);
+        #Add-Content  $tourfile "</scene>"; 
+        }
+        # Tail
+        Add-Content $tourfile  "</krpano>"
+    }
+}
