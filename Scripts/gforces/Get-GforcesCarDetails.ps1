98994db526aba360efc353ab3d98bd5751af3cbe
RafaelGP <mr.rafaelgp@gmail.com>
2014-12-09 18:06:53 +0000

    Get-GforcesCarDetails: First commit

---
+++ Scripts/gforces/Get-GforcesCarDetails.ps1
@@ -0,0 +1,18 @@
+﻿$paths = $(ll -path "E:\virtual_tours\gforces\cars\" -Exclude ".no_scenes", ".src", "brands", "shared", "crossdomain.xml" ).basename
+$item =
+foreach ($path in $paths){
+    $countrycode = ($path -split "_")[0]
+    $make = ($path -split "_")[-3]
+    $model = ($path -split "_")[-2]
+    $description = ($path -split "_")[-1]
+
+    new-object psobject -property  @{
+        Countrycode = $countrycode;        
+        Make = $make
+        Model = $model
+        Description = $description
+        } 
+
+  } 
+  #$item
+  $item | select -Property Countrycode,Make,Model,Description 
