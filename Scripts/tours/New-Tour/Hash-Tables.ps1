336d8780562297f3b9dd7d4436f2c3e66d569ac2
RafaelGP <mr.rafaelgp@gmail.com>
2015-04-04 17:45:08 +0100

    Created new directory 'scripts/tours/' and move all the relevant scripts there

---
+++ Scripts/tours/New-Tour/Hash-Tables.ps1
@@ -0,0 +1,19 @@
+﻿    # ---------- HASH TABLE METHOD ----------
+    #$Tours = $null
+    #$Tours = @{}
+    #$ToursArray | ForEach-Object  {
+    #    $tourName = $(Get-Variable $_).Name
+    #    $tourScenes = $(Get-Variable $_).Value
+    #    $Tours.add($tourName,$tourScenes)
+    #}   
+    # Print Tours information from the hash table  
+    #write-verbose "Number of Tours: $($Tours.count)"
+    #foreach ($pair in  $Tours.GetEnumerator()) {
+    #    Write-Verbose "  $($pair.key) : $($pair.Value)"
+    #}
+
+    # Print Tours information from the arrays
+    #Write-Verbose "Virtual Tours list: ($($ToursArray.count))"
+    #$ToursArray | ForEach-Object {
+    #    Write-Verbose "  $_ : $($(Get-Variable $_).Value)"
+    #}
