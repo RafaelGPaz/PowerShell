b64daf5bbd0ddf16cbaeb038b2ada79a4e3df07a
RafaelGP <mr.rafaelgp@gmail.com>
2015-04-04 17:19:48 +0100

    Compare-GforcesCars > Rewrite the script completely to actually make it work

--- Scripts/gforces/Compare-GforcesCars.ps1
+++ Scripts/gforces/Compare-GforcesCars.ps1
@@ -1,14 +1,56 @@
 ﻿<#
 .Synopsis
-   Compare the folders inside my local 'gforces/cars/' directory and the 'interiors' directory in Amazon's server
+   Compare the config.XML file from Amazon server with my cars/.src/config.file
+   The Amazon file is generated every several days
+   The result excludes the cars with the 'countrycode' IE as they're duplicated created by Gforces
 .EXAMPLE
-   Compare-GforcesCars.ps1
+    Run from any directory
+   Compare-GforcesCars
 #>
 
-[xml]$xmlFile=Invoke-WebRequest 'https://s3-eu-west-1.amazonaws.com/autofs/shared/interiors/v10setup/interiors.xml'
-$remoteFolders= $xmlFile.TOURS.ChildNodes.FOLDER_NAME
-$localFolders = $(Get-ChildItem -path "E:\virtual_tours\gforces\cars\" -Exclude ".no_scenes", ".src", "brands", "shared", "crossdomain.xml" ).basename
+[CmdletBinding()]
+Param()
 
-foreach ($carFolder in $localFolders){
-    
+Begin
+{
+    $ErrorActionPreference = "Stop"
+    # Variables
+    $gforcesList = "https://s3-eu-west-1.amazonaws.com/autofs/shared/interiors/v10setup/interiors.xml"
+    $config = "E:\virtual_tours\gforces\cars\.src\config.xml"
+    [Array]$tourIDArray = ""
+    [xml]$configXml = Get-Content $config
+    foreach ( $country in $configXml.tour.country ) {
+        foreach ( $brand in $country.brand) {
+            foreach ($model in $brand.model) {
+                foreach ($car in $model.car) {
+                    $tour = $($car.id) 
+                    [Array]$tourIDArray += $tour
+                }
+            }
+        }
+    }
+    foreach ( $rename in $configXml.tour.rename ) {
+        foreach ( $car in $rename.car ) {
+            $renameTo = $($car.renameTo)
+            [Array]$tourIDArray += $renameTo
+        }
+    }
+    Write-Output "No of shot cars in config.xml --------------> $($($tourIDArray).count)"
+
+    [xml]$xml = (new-object System.Net.WebClient).DownloadString($gforcesList)
+    #[xml]$xml = get-content "C:\Users\Rafael\Downloads\interiors.xml"
+    $xmlFilter = $xml.TOURS.ChildNodes | where { $_.countrycode -notlike "IE" }
+    $gforcesXml = $xmlFilter.folder_name 
+    Write-Output "No of shot cars in gforces.xml --------------> $($($gforcesXml).count)"
+    #$($gforcesXml.TOURS.ChildNodes).folder_name
+
+    $diferences = Compare-Object $($gforcesXml) $($tourIDArray)
+    Write-Output "gforces.CVS vs config.XML:"
+    $diferences
+
+
+}
+End
+{
+    Write-Output "_EOF_"
 }
