0d1cb625658a37b2cefda6cfae39d3ec4a6be1c4
RafaelGP <mr.rafaelgp@gmail.com>
2015-05-14 10:50:42 +0100

    Upgraded Run-Backup script to turn it into a proper script.
    Now it offers 4 options to backup different directories

--- Scripts/Run-Backup.ps1
+++ Scripts/Run-Backup.ps1
@@ -1,16 +1,104 @@
-write-verbose "Backing up Home img ..." -Verbose
-$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\home_img.ffs_batch")
-$batfile.WaitForExit()
-write-verbose "Backing up Home plus ..." -Verbose
-$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\home_plus.ffs_batch")
-$batfile.WaitForExit()
-write-verbose "Backing up Work img ..." -Verbose
-$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\work_img.ffs_batch")
-$batfile.WaitForExit()
-write-verbose "Backing up Work plus ..." -Verbose
-$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\work_plus.ffs_batch")
-$batfile.WaitForExit()
+<#
+.Synopsis
+   Backup System
+.DESCRIPTION
+    There are three options:
+    1- Home: Backup only the Home directory
+    2- Work: Backup only the E:/ drive
+    3- All: Backup both Home and Work
+    4- VirtualBox - Backup VirtualBox directory
+.EXAMPLE
+   Run-Backup
+#>
+
+[cmdletbinding()]
+
+    Param (
+        [Parameter(
+            Mandatory=$False,
+            ValueFromPipeline=$false)]
+        [Switch] $IgnoreUndercores
+        )
+
+$VerbosePreference = "Continue"
+# Stop if there is any error
+$ErrorActionPreference = "Stop"
+
+#write-verbose "Backing up Home img ..." -Verbose
+
+#write-verbose "Backing up Home plus ..." -Verbose
+#$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\home_plus.ffs_batch")
+#$batfile.WaitForExit()
+#write-verbose "Backing up Work img ..." -Verbose
+#$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\work_img.ffs_batch")
+#$batfile.WaitForExit()
+#write-verbose "Backing up Work plus ..." -Verbose
+#$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\work_plus.ffs_batch")
+#$batfile.WaitForExit()
 #write-verbose "Backing up Dropbox ..." -Verbose
 #$batfile = [diagnostics.process]::Start("E:\documents\freefilesync\dropbox.ffs_batch")
 #$batfile.WaitForExit()
-write-verbose "[i] All backed up" -Verbose
+
+function Backup-Home {
+    param()
+    write-verbose "Backing up Home img ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\home_img.ffs_batch")
+    $batfile.WaitForExit()
+    write-verbose "Backing up Home plus ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\home_plus.ffs_batch")
+    $batfile.WaitForExit()
+}
+
+function Backup-Work {
+    param()
+    write-verbose "Backing up Work img ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\work_img.ffs_batch")
+    $batfile.WaitForExit()
+    write-verbose "Backing up Work plus ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\work_plus.ffs_batch")
+    $batfile.WaitForExit()
+}
+
+function Backup-All {
+    Backup-Home
+    Backup-Work
+}
+
+function Backup-Work {
+    param()
+    write-verbose "Backing up VirtualBox ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\virtualbox.ffs_batch")
+    $batfile.WaitForExit()
+}
+
+Clear-Host
+$conf_dir = "E:\documents\freefilesync"
+
+# Choose what to backup   
+$title = "Backup"
+$message = "What would you lite to backup?"
+
+$one = New-Object System.Management.Automation.Host.ChoiceDescription "&Home", `
+    "Home"
+
+$two = New-Object System.Management.Automation.Host.ChoiceDescription "&Work", `
+    "Work"
+
+$three = New-Object System.Management.Automation.Host.ChoiceDescription "&All", `
+    "All"
+
+$four = New-Object System.Management.Automation.Host.ChoiceDescription "&VirtualBox", `
+    "VirtualBox"
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($one, $two, $three, $four)
+
+$result = $host.ui.PromptForChoice($title, $message, $options, 2) 
+
+Switch( $result ){
+    0{ Backup-Home }
+    1{ Backup-Work }
+    2{ Backup-All }
+    3{ Backup-VirtualBox }
+}
+
+Write-Verbose "EOF" -Verbose
