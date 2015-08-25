fd8fa7228de361c77b5a806fc3b80ae830a90c60
rafaelgp <rafael@debian>
2015-08-25 14:46:17 +0100

    Changed 'halt' alias for 'off'

--- conf/posh_aliases.ps1
+++ conf/posh_aliases.ps1
@@ -5,7 +5,7 @@ Set-Alias lf List-FilesOnly
 Set-Alias ldf List-DotFilesOnly
 Set-Alias cc Clear-Host
 Set-Alias sls Select-String
-Set-Alias halt Stop-Computer
+Set-Alias off Stop-Computer
 Set-Alias pro Reload-Profile
 Set-Alias aws "C:\Python34\Scripts\aws.cmd"
 Set-Alias nf New-File
