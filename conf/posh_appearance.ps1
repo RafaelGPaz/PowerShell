26396132b6f741768d40822031468a400ac6f839
Rafael GP <Rafael GP>
2015-12-21 16:34:45 +0000

    Updated prompt like the one used in cmder

--- conf/posh_appearance.ps1
+++ conf/posh_appearance.ps1
@@ -1,21 +1,42 @@
 ﻿# PowerShell appearance
 
-# Set up a simple prompt showing the current directory
-function prompt {
-    if ( $Host.Name -eq "ConsoleHost" ) {
-        # Rename tab with the name of the current directory
-        & "$env:ConEmuBaseDir\ConEmuC.exe" "/GUIMACRO", 'Rename(0,@"'$(get-item $PWD).BaseName'")' > $null
-	    Write-Host PS ($pwd) -nonewline -ForegroundColor Cyan
+try {
+    # Check if git is on PATH, i.e. Git already installed on system
+    Get-command -Name "git" -ErrorAction Stop >$null
+} catch {
+    $env:Path += ";$env:CMDER_ROOT\vendor\git-for-windows\bin"
+}
+
+try {
+    Import-Module -Name "posh-git" -ErrorAction Stop >$null
+    $gitStatus = $true
+} catch {
+    Write-Warning "Missing git support, install posh-git with 'Install-Module posh-git' and restart cmder."
+    $gitStatus = $false
+}
+
+function checkGit($Path) {
+    if (Test-Path -Path (Join-Path $Path '.git/') ) {
+        Write-VcsStatus
+        return
     }
-    if ( $Host.Name -eq "Windows PowerShell ISE Host" ) {
-	    Write-Host PS ($pwd) -nonewline -ForegroundColor Magenta
+    $SplitPath = split-path $path
+    if ($SplitPath) {
+        checkGit($SplitPath)
     }
-	# Git Prompt
-	if($Global:GitPromptSettings.EnablePromptStatus) {
-	    $Global:GitStatus = Get-GitStatus
-	    Write-GitStatus $GitStatus
-	}
-	return " > "
+}
+
+# Set up a Cmder prompt, adding the git prompt parts inside git repos
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+    $Host.UI.RawUI.ForegroundColor = "White"
+    Write-Host $pwd.ProviderPath -NoNewLine -ForegroundColor Green
+    if($gitStatus){
+        checkGit($pwd.ProviderPath)
+    }
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-Host "`nλ" -NoNewLine -ForegroundColor "DarkGray"
+    return " "
 }
 
 # Set Terminal colors
