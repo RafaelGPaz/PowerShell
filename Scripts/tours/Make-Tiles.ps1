c6190ab927d05544730da5e348dcfcd7c3a44f56
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2016-04-15 16:20:47 +0100

    Merge-XmlFiles.ps1 > Added an option to create tiles for Clarendon and Gforces V10 Visualiser

--- Scripts/tours/Make-Tiles.ps1
+++ Scripts/tours/Make-Tiles.ps1
@@ -50,6 +50,8 @@ function run-krpano {
 }
 
 function make-tiles {
+    # $type possible values: normal, gforces. Defined in funciton 'make-threelevels'
+    Param($type)
     # Check panos directory is not empty
     if ($(Get-ChildItem .\.src\panos\) -eq $null) { Throw ".src\panos\ directory doesn't contain any folder" }
     foreach ( $tour in dir ".\.src\panos\" ) {
@@ -76,8 +78,13 @@ function make-tiles {
                 $outputfolder = "$($($panopath).Directory)\output"
                 $scenesfolder = "$outputfolder\scenes\"
                 $xmlfile = "$outputfolder\$panoname.xml"
-                $custompath = '%SWFPATH%/scenes/'
-                run-krpano -xmlfile $xmlfile -custompath $custompath -type "normal"
+                #if ($type -like "normal") {
+                $custompath = "%SWFPATH%/scenes/$panoname"
+                #}
+                #if ($type -like "gforces") {
+                #    $custompath = "%SWFPATH%/../$tour/files/scenes/$panoname"
+                #}
+                run-krpano -xmlfile $xmlfile -custompath $custompath -type $type
                 # Move new tiles folders and xml files to tour\files\scenes\
                 Move-Item ".\.src\panos\$tour\output\scenes\$panoname" ".\$tour\files\scenes\"
                 Move-Item -Force ".\.src\panos\$tour\output\$panoname.xml" ".\$tour\files\scenes\"
@@ -98,8 +105,15 @@ function make-twolevels {
 }
 
 function make-threelevels {
+    Param($type)
     $krconfig = "-config=$krdir\krpano_conf\templates\tv_tiles_for_cars_ipad.config"
-    make-tiles
+    if (!($type)) {
+    make-tiles -type "gforces"
+    }
+    else
+    {
+    make-tiles -type "normal"
+    }
 }
 
 function make-sitesurvey {
@@ -151,19 +165,25 @@ function make-sitesurvey {
 function make-gforces {
     Write-Verbose ('Running...')
     $krconfig = "-config=$krdir\krpano_conf\templates\tv_tiles_for_cars_ipad.config"
+    # Delete any residual files or folders
+    Get-ChildItem -Path ".src\panos\" -Recurse -include @("output","*.kro") | foreach { Remove-Item $_.FullName -Recurse }
     # Check if the tour folder contains any jpg files
-    if ($(Get-ChildItem .\.src\panos\*.jpg) -eq $null) { Throw ".src\panos\ doesn't contain any panoramas" }
+    if ($(Get-ChildItem .\.src\panos\*) -eq $null) { Throw ".src\panos\ doesn't contain any panoramas" }
     # Check that the panorama names has 3 underscores, unless this is ignored with the switch '-IgnoreUnderscores'
     if (!($IgnoreUndercores)) {
-        Get-ChildItem ".\.src\panos\*.jpg" |
+        Get-ChildItem ".\.src\panos\*" |
         foreach {
             $underscores = ((($_.basename).ToString()).split("_")).count
             if($underscores -ne "4") { Throw "The file $($_.BaseName) doesn't have 4 underscores, it has $underscores. Plaese raname it. "}
         }
     }
-    # Delete any residual file or folder
-    if(Test-Path ".\.src\panos\output") { Remove-Item -Recurse -Force ".\.src\panos\output" }
-    Get-ChildItem ".\.src\panos\*.kro" | foreach { Remove-Item $_.Fullname }
+    # Check for duplicates
+    $Names = @{}
+    foreach($panoname in $(dir ".\.src\panos\*").BaseName) {
+        $Names[$panoname] += 1
+    }
+    $duplicated = ($Names.GetEnumerator() | Where-Object{ $_.Value -gt 1 })
+    if ($duplicated) { Throw "The following tours are duplicated: $($duplicated.Name)"}
     $quantity = 0
     # Create tiles
     foreach ( $panoname in $(dir ".\.src\panos\*.jpg").BaseName |
@@ -174,10 +194,6 @@ function make-gforces {
         $panopath = Get-Item ".\.src\panos\$panoname.jpg"
         # Check if there is a folder containing the scene tiles AND it's corresponding xml file
         if(!(Test-Path -PathType Container ".\$panoname\files\scenes\tiles") -and !(Test-Path ".\$panoname\files\scenes\scene.xml"))
-        #{
-        #    Write-Verbose ('  [ OK ] ' + $panoname)
-        #}
-        #else
         {
             Write-Verbose ('  [    ] Making tiles for scene: ' + $panoname)
             check-folder -dir "$panoname"
@@ -197,6 +213,59 @@ function make-gforces {
     # Delete output dir if exists
     if((Test-Path -PathType Container ".\.src\panos\output") -eq $true) {
         Remove-Item -Recurse .\.src\panos\output
+    }
+}
+
+function make-clarendon {
+    $krconfig = "-config=$krdir\krpano_conf\templates\tv_tiles_for_cars_ipad.config"
+    # Check panos directory is not empty
+    if ($(Get-ChildItem .\.src\panos\) -eq $null) { Throw ".src\panos\ directory doesn't contain any folder" }
+    foreach ( $zone in dir ".\.src\panos\" ) {
+        #Write-Verbose ('Zone: ' + $zone)
+        if ($(Get-ChildItem .\.src\panos\$zone) -eq $null) { Throw ".src\panos\$zone directory doesn't contain any folder" }
+        foreach ( $tour in dir ".\.src\panos\$zone\" ) {
+            #Write-Verbose ('Tour: ' + $tour)
+            if ($(Get-ChildItem .\.src\panos\$zone) -eq $null) { Throw ".src\panos\$zone directory doesn't contain any folder" }
+            foreach ( $apartment in dir ".\.src\panos\$zone\$tour\" ) {
+                #Write-Verbose "$zone, $tour, $apartment"               
+                # Check if the tour folder contains any jpg files
+                if ($(Get-ChildItem .\.src\panos\$zone\$tour\$apartment\*.jpg) -eq $null) { Throw ".src\panos\$zone\$tour\$apartment doesn't contain any panoramas" }
+                foreach ( $panoname in $(dir ".\.src\panos\$zone\$tour\$apartment\*.jpg").BaseName |
+                # The first RegEx removes all the numbers and then sorts the list based on just the letters and punctuation.
+                # The second RegEx removes all letters and punctuation leaving just the numbers.
+                # Then casts the numbers to an Integer and sorts again.
+                Sort-Object -Property {$_-replace '[\d]'},{$_-replace '[a-zA-Z\p{P}]'-as [int]} ) { 
+                    $panopath = Get-Item ".\.src\panos\$zone\$tour\$apartment\$panoname.jpg"
+                    # Check if there is a folder containing the scene tiles AND it's corresponding xml file
+                    if((Test-Path -PathType Container ".\$zone\$tour\$apartment\files\scenes\$panoname") -eq $True -and (Test-Path ".\$zone\$tour\$apartment\files\scenes\$panoname.xml"))
+                    {
+                        Write-Verbose ('  [ OK ] ' + $zone + ' > ' + $tour + ' > ' + $apartment + ' > ' + $panoname)
+                    }
+                    else 
+                    {    
+                        Write-Verbose ('  -----> Making tiles for scene: ' + $zone + ' > ' + $tour + ' > ' + $apartment + ' > ' + $panoname)
+                        check-folder -dir "$zone"
+                        check-folder -dir "$zone\$tour"
+                        check-folder -dir "$zone\$tour\$apartment"
+                        check-folder -dir "$zone\$tour\$apartment\files"
+                        check-folder -dir "$zone\$tour\$apartment\files\scenes"
+                        $outputfolder = "$($($panopath).Directory)\output"
+                        $scenesfolder = "$outputfolder\scenes\"
+                        $xmlfile = "$outputfolder\$panoname.xml"
+                        $custompath = "%SWFPATH%/../../$apartment/files/scenes/"
+                        run-krpano -xmlfile $xmlfile -custompath $custompath -type "normal"
+                        # Move new tiles folders and xml files to tour\files\scenes\
+                        Move-Item ".\.src\panos\$zone\$tour\$apartment\output\scenes\$panoname" ".\$zone\$tour\$apartment\files\scenes\"
+                        Move-Item -Force ".\.src\panos\$zone\$tour\$apartment\output\$panoname.xml" ".\$zone\$tour\$apartment\files\scenes\"
+                        Write-Verbose '  Tiles Done'
+                        # Delete output dir if exists
+                        if((Test-Path -PathType Container ".\.src\panos\$zone\$tour\$apartment\output") -eq $true) {
+                            Remove-Item -Recurse ".\.src\panos\$zone\$tour\$apartment\output"
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -222,7 +291,9 @@ $three = New-Object System.Management.Automation.Host.ChoiceDescription "&Site S
     "Site Servey"
 $four = New-Object System.Management.Automation.Host.ChoiceDescription "&Gforces", `
     "Gforces"
-$options = [System.Management.Automation.Host.ChoiceDescription[]]($one, $two, $three, $four)
+$five = New-Object System.Management.Automation.Host.ChoiceDescription "&Clarendon", `
+    "Clarendon"
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($one, $two, $three, $four, $five)
 
 $result = $host.ui.PromptForChoice($title, $message, $options, 1) 
     
@@ -231,5 +302,6 @@ Switch( $result ){
     1{ make-threelevels }
     2{ make-sitesurvey }
     3{ make-gforces }
+    4{ make-clarendon }
 }
 Write-Verbose "EOF"
