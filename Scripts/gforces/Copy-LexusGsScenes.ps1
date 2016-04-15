cedf8d37f759e6b0dd3ea32f91bc3bc2b2f5a9d9
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2016-04-15 16:23:23 +0100

    New Copy-LexusGsScenes.ps1 and Update-LexusGsSceneFiles.ps1 scripts

---
+++ Scripts/gforces/Copy-LexusGsScenes.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.DESCRIPTION
+  
+.EXAMPLE
+  
+#>
+
+[cmdletbinding()]
+
+    Param (
+        [Parameter(
+            Mandatory=$False,
+            ValueFromPipeline=$false)]
+        [Switch] $IgnoreUndercores
+        )
+
+$VerbosePreference = "Continue"
+# Stop if there is any error
+$ErrorActionPreference = "Stop"
+$root = 'E:\virtual_tours\gforces\manufacturers\lexus_manufacturer'
+$selectedFolder = Get-Childitem $root\ae_lexus_gs350_2016\ae_lexus_gs350_2016\files\scenes
+
+Write-Verbose "[    ] Lexus GS350 Base"
+#Copy-Item -Recurse -Force $root\ae_lexus_gs350_base_2016\ae_lexus_gs350_base_2016\files\scenes\* $root\ae_lexus_gs350_2016\ae_lexus_gs350_2016\files\scenes\base\
+Write-Verbose "[ OK ]"
+Write-Verbose "[    ] Lexus GS350 FSport"
+#Copy-Item -Recurse -Force $root\ae_lexus_gs350_fsport_2016\ae_lexus_gs350_fsport_2016\files\scenes\* $root\ae_lexus_gs350_2016\ae_lexus_gs350_2016\files\scenes\fsport\
+Write-Verbose "[ OK ]"
+Write-Verbose "[    ] Lexus GS350 Luxury"
+#Copy-Item -Recurse -Force $root\ae_lexus_gs350_luxury_2016\ae_lexus_gs350_luxury_2016\files\scenes\* $root\ae_lexus_gs350_2016\ae_lexus_gs350_2016\files\scenes\luxury\
+Write-Verbose "[ OK ]"
+Write-Warning "Run Update-LexusGsSceneFiles.ps1"
+Write-Warning "Run Convert_dubai_xmlfiles_to_dos.sh"
+Write-Warning "Run merge-lexusxmlfiles.sh"
+
+Write-Verbose "EOF"
---
+++ Scripts/gforces/Update-LexusGsSceneFiles.ps1
@@ -0,0 +1,39 @@
+﻿<#
+.DESCRIPTION
+  
+.EXAMPLE
+  
+#>
+
+[cmdletbinding()]
+
+    Param (
+        [Parameter(
+            Mandatory=$False,
+            ValueFromPipeline=$false)]
+        [Switch] $IgnoreUndercores
+        )
+
+$VerbosePreference = "Continue"
+# Stop if there is any error
+$ErrorActionPreference = "Stop"
+$root = 'E:\virtual_tours\gforces\manufacturers\lexus_manufacturer\ae_lexus_gs350_2016\ae_lexus_gs350_2016\files\scenes'
+$selectedFolder = Get-Childitem $root
+
+foreach ($carModel in $selectedFolder) {
+    Write-Verbose $carModel
+    foreach ($xmlFile in Get-ChildItem $root\$carModel -Filter "*.xml") {
+        #write-host $xmlFile.FullName
+        $template_content = Get-Content $xmlFile.FullName
+        #Write-Host $template_content
+        $sceneName = 'name="' + $($carModel) + '_scene'
+        $tilesPath = 'scenes/' + $($carModel) + '/scene'
+        #write-host = $sceneName
+        $template_content |
+        foreach { ($_).replace('name="scene',$sceneName)} |
+        foreach { ($_).replace('scenes/scene',$tilesPath)} |
+        Out-File -Encoding utf8 $xmlFile.FullName
+    }
+}
+
+Write-Verbose "EOF"
