fc348e9a20adcaa628195bb412fb14dc57f63aab
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2016-04-15 16:22:31 +0100

    Merge-XmlFiles.ps1 > Completely updated the script

--- Scripts/tours/Merge-XmlFiles.ps1
+++ Scripts/tours/Merge-XmlFiles.ps1
@@ -1,78 +1,146 @@
 ﻿<#
 .DESCRIPTION
-    Create a new tour.xml file merging all the XML files in the following directories:
-    - files/content
-    - files/include
-    - files/scenes
+    Create tour.xml file for each tour folder in the current directory
+    
+    tour.xml merge all the XML files contained in:
+    1- './tour/files/content/*.xml'
+    2- './shared/include/*/index.xml' directory if exists
+    3- './tour/files/include/*/index.xml'
+    4- './tour/files/scenes/*.xml'
+    
+    Run the script from the same directory than the folder '.src'
+    
 #>
-[CmdletBinding()]             
-    Param (                        
-        [Parameter(Mandatory=$False, 
-            Position=0)]  
-        [String]$Param1
-        )#End Param
-if(!(Test-Path -Path ".\files")) { Throw "There is no 'files' directory. Are you in the right directory?" }
-$tourfile=".\files\tour.xml"
-if(!(Test-Path -Path $tourfile)) { Throw "There is no 'tour.xml' file" }
-rm $tourfile
-New-Item -ItemType File -path $tourfile | Out-Null
-# Head
-set-content $tourfile  '<krpano version="1.18" showerrors="false"><krpano logkey="true" />'
-
-# Plugins Directory
-if(Test-Path -Path ".\files\plugins") {
-    $plugins = Get-ChildItem .\files\plugins\*.xml
-    Write-Output '===== Plugins ====='
-    Write-Output $plugins.Name 
-    ForEach ($file in $plugins) {Add-Content $tourfile $(Get-Content $file.FullName | 
-    where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_ -notmatch '<?xml version' -and $_.trim() -ne "" } |
-    foreach {$_.ToString().TrimStart() }  
-    Out-String) }
+[CmdletBinding()]
+Param (
+[Parameter(
+    Mandatory=$False,
+    ValueFromPipeline=$True,
+    ValueFromPipelineByPropertyName=$true)]
+    $TourName
+    )
+Begin {
+    #$DebugPreference = "Continue"
+    # Stop if there is any error
+    $ErrorActionPreference = "Stop"
+    $krVersion = "1.18"
+    $dir = $pwd
+    if(!(Test-Path -Path "$dir\.src")) { Throw "There is no '.src' directory. Are you in the right directory?" }
+    Write-Verbose "Checking files and folders...."
+    foreach ( $tour in $(Get-ChildItem "$dir" -Exclude ".custom", ".src", ".no_scenes", "brands", "shared" -Directory) ) {
+        if(!(Get-ChildItem "$dir\.src\panos\$($tour.BaseName)\*.jpg" )) { Throw "There are no JPG panoramos in the directory '.src/panos'" }
+        [Array]$toursArray += $tour
+        foreach ( $scene in $(Get-ChildItem "$dir\.src\panos\$($tour.BaseName)\*.jpg")) {
+            if(!(Test-Path "$dir\$($tour.BaseName)\files" )) { Throw "Folder '$dir\$($tour.BaseName)\files' NOT FOUND. Did you create the tiles correctly?" }
+            if(!(Test-Path "$dir\$($tour.BaseName)\files\scenes" )) { Throw "Folder '$dir\$($tour.BaseName)\files\scenes' NOT FOUND. Did you create the tiles correctly?" }
+            if(!(Test-Path "$dir\$($tour.BaseName)\files\scenes\$($scene.BaseName)" )) { Throw "Folder '$dir\$($tour.BaseName)\files\scenes\$($scene.BaseName)' NOT FOUND. Did you create the tiles correctly?" }
+            if(!(Test-Path "$dir\$($tour.BaseName)\files\scenes\$($scene.BaseName).xml" )) { Throw "File .\$($tour.BaseName)\files\scenes\$($scene.BaseName).xml NOT FOUND. Did you create the tiles correctly?" }
+        }
+    }
+     # Check there aren't folders or files which don't belong to an existing panorama
+    foreach ( $tour in $(Get-ChildItem "$dir" -Exclude ".custom", ".src", ".no_scenes", "brands", "shared" -Directory) ) {
+        foreach ( $scene in $(Get-ChildItem "$dir\$($tour.BaseName)\files\scenes\" -Directory )) {
+            if(!(Test-Path ".src\panos\$($tour.BaseName)\$($scene.BaseName).jpg" )) {
+                throw "The following folder is obsolete: '.\$($tour.BaseName)\files\scenes\$($scene.BaseName)'"
+            }
+        }
+        foreach ( $scene in $(Get-ChildItem "$dir\$($tour.BaseName)\files\scenes\*.xml" )) {
+            if(!(Test-Path ".src\panos\$($tour.BaseName)\$($scene.BaseName).jpg" )) {
+                throw "The following file is obsolete: '.\$($tour.BaseName)\files\scenes\$($scene.BaseName).xml'"
+            }
+        }   
+    }
+    
+    function Clean-TourXml ($selectedFolder) {
+        foreach ($xmlFile in $selectedFolder) {
+            Get-Content $xmlFile |
+            # Skip the lines containing krpano tags
+            where { $_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_.trim() -ne "" } |
+            # Remove any whitespace before ="
+            foreach { $_ -replace '\s+="','="' } |
+            # Remove any whitespace at the start of each line. Do this always the last thing in this function
+            foreach { $_.ToString().TrimStart() |
+            Add-Content $tourFile
+            }
+        }
+    }
 }
 
-# Content Directory
-if(!(Test-Path -Path ".\files\content")) { rm $tourfile; Throw "There is no 'content' directory" 
+Process {
+    if (!($tourName)) {
+        $tourName = $toursArray
+    }
+    Write-Verbose "$($tourName.BaseName)"
+    foreach ($tour in $tourName) {
+        $tourFile = "$dir\$($tour.BaseName)\files\tour.xml"
+        New-Item -ItemType File $tourFile -Force | Out-Null
+        #<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<krpano version=\"$krpano_version\" onstart=\"startup();\" showerrors=\"false\">
+        $head = '<?xml version="1.0" encoding="UTF-8"?><krpano version="' + $krVersion + '" logkey="false" showerrors="false" onstart="startup();" >'
+        Add-Content $tourFile $head
+        # PLUGINS
+        if(Test-Path "$dir\shared") {
+            if (Get-ChildItem "$dir\shared\plugins\*.xml") {
+                Write-Verbose "    PLUGINS:"
+                foreach ($xmlFile in $(Get-ChildItem "$dir\shared\plugins\*.xml" )) {
+                    Write-Verbose "    [ OK ] $($xmlFile.Directory.Basename)/$($xmlFile.BaseName).xml"
+                    $pluginsFolder = $xmlFile
+                    Clean-TourXml $pluginsFolder             
+                }
+            }
+        }
+        else
+        {
+            if (Get-ChildItem "$dir\$($tour.BaseName)\files\plugins\*.xml") {
+                Write-Verbose "    PLUGINS:"
+                foreach ($xmlFile in $(Get-ChildItem "$dir\$($tour.BaseName)\files\plugins\*.xml" )) {
+                    Write-Verbose "    [ OK ] $($xmlFile.Directory.Basename)/$($xmlFile.BaseName).xml"
+                    $pluginsFolder = $xmlFile
+                    Clean-TourXml $pluginsFolder             
+                }
+            }
+        }
+        # CONTENT
+        Write-Verbose "    CONTENT:"
+        foreach ($xmlFile in $(Get-ChildItem "$dir\$($tour.BaseName)\files\content\*.xml")) {
+            Write-Verbose "    [ OK ] $($xmlFile.Directory.Basename)/$($xmlFile.BaseName).xml"
+            Clean-TourXml $xmlFile
+        }
+        # INCLUDE
+        if(Test-Path "$dir\shared") {
+            if (Get-ChildItem "$dir\shared\include\*\*.xml") {
+                Write-Verbose "    INCLUDE:"
+                foreach ($xmlFile in $(Get-ChildItem "$dir\shared\include\*\*.xml" -Exclude coordfinder, editor_and_options)) {
+                    Write-Verbose "    [ OK ] $($xmlFile.Directory.Basename)/$($xmlFile.BaseName).xml"
+                    $includeFolder = $xmlFile
+                    Clean-TourXml $includeFolder             
+                }
+            }
+        }
+        else
+        {
+            if (Get-ChildItem "$dir\$($tour.BaseName)\files\include\*\*.xml") {
+                Write-Verbose "    INCLUDE:"
+                foreach ($xmlFile in $(Get-ChildItem "$dir\$($tour.BaseName)\files\include\*\*.xml" -Exclude coordfinder, editor_and_options)) {
+                    Write-Verbose "    [ OK ] $($xmlFile.Directory.Basename)/$($xmlFile.BaseName).xml"
+                    $includeFolder = $xmlFile
+                    Clean-TourXml $includeFolder             
+                }
+            }
+        }
+        # SCENES        
+        if (Get-ChildItem "$dir\$($tour.BaseName)\files\scenes\*.xml") {
+            Write-Verbose "    SCENES:"
+            foreach ($xmlFile in $(Get-ChildItem "$dir\$($tour.BaseName)\files\scenes\*.xml")) {
+                Write-Verbose "    [ OK ] $($xmlFile.Directory.Basename)/$($xmlFile.BaseName).xml"
+                $scenesFolder = $xmlFile
+                Clean-TourXml $scenesFolder             
+            }
+        }
+        Add-Content $tourFile '</krpano>'
+    }
 }
-$content = Get-ChildItem .\files\content\*.xml
-if($content -eq $null) { rm $tourfile; Throw "There are no xml files in the 'content' directory" }
-Write-Output '===== Content ====='
-Write-Output $content.Name 
-ForEach ($file in $content) {Add-Content $tourfile $(Get-Content $file.FullName | 
-where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_ -notmatch '<?xml version' -and $_.trim() -ne "" } |
-foreach {$_.ToString().TrimStart() }  
-Out-String) }
 
-# Incude Directory
-if(!(Test-Path -Path ".\files\include")) { rm $tourfile; Throw "There is no 'include' directory" }
-$include = Get-ChildItem .\files\include\*\*.xml -Exclude 'coordfinder', 'editor_and_options'
-if($include -eq $null) { rm $tourfile; Throw "There are no xml files in the 'include' directory" }
-Write-Output '===== Include ====='
-#Write-Output =  $include.Directory.Name
-ForEach ($file in $include) {
-Write-Output "$($file.Directory.Name)/index.xml"
-Add-Content $tourfile $(Get-Content $file.FullName | 
-where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_ -notmatch '<?xml version' -and $_.trim() -ne "" } |
-foreach {$_.ToString().TrimStart() }  
-Out-String ) }
-
-# Scenes Directory
-if(!(Test-Path -Path ".\files\scenes")) { rm $tourfile; Throw "There is no 'scenes' directory" }
-#$scenes = Get-ChildItem .\files\scenes\*.xml
-$scenes = Get-ChildItem .\files\scenes\*.xml -Exclude info*.xml
-if($scenes -eq $null) { rm $tourfile; Throw "There are no xml files in the 'scenes' directory" }
-Write-Output '===== Scenes ====='
-Write-Output $scenes.Name
-ForEach ($file in $scenes) {
-# Open scene tag
-#Add-Content  $tourfile "<scene name=`"$($_.BaseName)`">";  
-# scenes\scene#.xml
-Add-Content $tourfile $(Get-Content $file.FullName |
-where {$_ -notmatch "<krpano" -and $_ -notmatch "</krpano" -and $_.trim() -ne "" } |
-#foreach {$_.ToString().TrimStart().Replace("url=`"scene","url=`"`%SWFPATH`%/scenes/scene") } |
-foreach {$_.ToString().TrimStart() } |
-Out-String);
-#Add-Content  $tourfile "</scene>"; 
+End {
+    Write-Verbose "_EOF_"
 }
-# Tail
-Add-Content $tourfile  "</krpano>"
-Write-Output "_EOF_"
+
