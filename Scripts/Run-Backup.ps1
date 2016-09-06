f44e54dd222488520ad43fd2fa561278f6f7a197
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2016-09-06 06:55:13 +0100

    Run-Backup > Edited to run IMG and PLUS backups. Deleted VBox Backup

--- Scripts/Run-Backup.ps1
+++ Scripts/Run-Backup.ps1
@@ -3,10 +3,9 @@
    Backup System
 .DESCRIPTION
     There are three options:
-    1- Home: Backup only the Home directory
-    2- Work: Backup only the E:/ drive
-    3- All: Backup both Home and Work
-    4- VirtualBox - Backup VirtualBox directory
+    1- IMG: Image backup of HOME directory and E:/ drive (WORK)
+    2- PLUS: Incremental backup of HOME directory and E:/ drive (WORK)
+    3- All: IMG and PLUS backups
 .EXAMPLE
    Run-Backup
 #>
@@ -24,36 +23,29 @@ $VerbosePreference = "Continue"
 # Stop if there is any error
 $ErrorActionPreference = "Stop"
 
-function Backup-Home {
+function Img-Backup {
     param()
-    write-verbose "Backing up Home img ..." -Verbose
+    write-verbose "HOME IMG Backup in process ..." -Verbose
     $batfile = [diagnostics.process]::Start("$conf_dir\home_img.ffs_batch")
     $batfile.WaitForExit()
-    write-verbose "Backing up Home plus ..." -Verbose
-    $batfile = [diagnostics.process]::Start("$conf_dir\home_plus.ffs_batch")
+    write-verbose "WORK IMG backup in process ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\work_img.ffs_batch")
     $batfile.WaitForExit()
 }
 
-function Backup-Work {
+function Plus-Backup {
     param()
-    write-verbose "Backing up Work img ..." -Verbose
-    $batfile = [diagnostics.process]::Start("$conf_dir\work_img.ffs_batch")
+    write-verbose "HOME PLUS Backup in process ..." -Verbose
+    $batfile = [diagnostics.process]::Start("$conf_dir\home_plus.ffs_batch")
     $batfile.WaitForExit()
-    write-verbose "Backing up Work plus ..." -Verbose
+    write-verbose "WORK PLUS Backup in process ..." -Verbose
     $batfile = [diagnostics.process]::Start("$conf_dir\work_plus.ffs_batch")
     $batfile.WaitForExit()
 }
 
 function Backup-All {
-    Backup-Home
-    Backup-Work
-}
-
-function Backup-VirtualBox {
-    param()
-    write-verbose "Backing up VirtualBox ..." -Verbose
-    $batfile = [diagnostics.process]::Start("$conf_dir\virtualbox.ffs_batch")
-    $batfile.WaitForExit()
+    Img-Backup
+    Plus-Backup
 }
 
 Clear-Host
@@ -72,18 +64,14 @@ $two = New-Object System.Management.Automation.Host.ChoiceDescription "&Work", `
 $three = New-Object System.Management.Automation.Host.ChoiceDescription "&All", `
     "All"
 
-$four = New-Object System.Management.Automation.Host.ChoiceDescription "&VirtualBox", `
-    "VirtualBox"
-
-$options = [System.Management.Automation.Host.ChoiceDescription[]]($one, $two, $three, $four)
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($one, $two, $three)
 
 $result = $host.ui.PromptForChoice($title, $message, $options, 2) 
 
 Switch( $result ){
-    0{ Backup-Home }
-    1{ Backup-Work }
+    0{ Img-Backup }
+    1{ Plus-Backup }
     2{ Backup-All }
-    3{ Backup-VirtualBox }
 }
 
 Write-Verbose "EOF" -Verbose
